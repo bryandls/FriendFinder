8ad3a01d98cbcde149815258d9403054f3d7b97a
bryandls <bryandls86@gmail.com>
2018-06-22 00:30:52 -0700

    push

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+
+var friendsList = [
+    {
+        "name":"Adriana Lima",
+        "photo":"https://scstylecaster.files.wordpress.com/2016/05/adriana-lima-beauty1.jpg",
+        "scores":[
+            1,
+            1,
+            3,
+            2,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        "name":"Megan Fox",
+        "photo":"https://www.telegraph.co.uk/content/dam/news/2016/08/10/megan_fox_trans_NvBQzQNjv4BqzQtRsqCGtNpZPa4ZcOFC3Giv9EzwWtFpjelzJ5Ke9vM.jpg?imwidth=450",
+        "scores":[
+            4,
+            1,
+            1,
+            3,
+            5,
+            1,
+            1,
+            5,
+            1,
+            1
+          ]
+    },
+    {
+        "name":"Jessica Alba",
+        "photo":"https://images.parents.mdpcdn.com/sites/parents.com/files/styles/width_360/public/images/wordpress/273/jessica-alba-headshot.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    }
+  ];
+  
+  module.exports = friendsList;
